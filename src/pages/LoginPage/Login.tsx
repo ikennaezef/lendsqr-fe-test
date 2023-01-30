c3050639a11f5421654ae517d6c180f25a093c0c
Ikenna <iykee0333@gmail.com>
2023-01-30 17:15:44 +0000

    made login page responsive

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.scss";
 
 const Login = () => {
@@ -15,10 +16,11 @@ const Login = () => {
 	return (
 		<div className="loginPage">
 			<div className="page_container">
-				<div className="loginPage_left">
-					<div className="logo_container">
+			<div className="logo_container">
 						<img src="/images/logo.svg" alt="lendsqr logo" />
 					</div>
+				<div className="page_grid">
+				<div className="loginPage_left">
 					<div className="illustration_img">
 						<img src="/images/sign-in-img.svg" alt="" />
 					</div>
@@ -27,30 +29,35 @@ const Login = () => {
 					<div>
 						<h2 className="welcome_title">Welcome!</h2>
 						<h5 className="subtitle">Enter details to login.</h5>
-						<form className="login_form" onSubmit={onSubmitForm}>
-							<input
-								autoComplete="off"
-								type="email"
-								placeholder="Email"
-								className="email_input"
-							/>
+						<form className="login_form" onSubmit={onSubmitForm} onSubmitCapture={onSubmitForm}>
+							<div className="email_input_container">
+								<input
+									autoComplete="off"
+									type="email"
+									placeholder="Email"
+									className="email_input"
+								/>
+							</div>
 							<div className="password_input_container">
 								<input
 									type={showPassword ? "text" : "password"}
 									placeholder="Password"
 								/>
-								<button id="show_password_btn" onClick={toggleShowPassword}>
+								<span id="show_password_btn" onClick={toggleShowPassword}>
 									{showPassword ? "HIDE" : "SHOW"}
-								</button>
+								</span>
 							</div>
 							<a href="#" className="forgot_password_link">
 								FORGOT PASSWORD?
 							</a>
 							<div className="login_btn_container">
-								<button id="login_btn">LOG IN</button>
+								<Link to="/dashboard">
+									<button id="login_btn">LOG IN</button>
+								</Link>
 							</div>
 						</form>
 					</div>
+				</div>
 				</div>
 			</div>
 		</div>
